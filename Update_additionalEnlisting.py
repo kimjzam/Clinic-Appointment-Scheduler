7d78fc2b4b37f540a5cda01c493e183abfc213e4
rrbdespabiladeras <54093540+rrbdespabiladeras@users.noreply.github.com>
2020-01-27 02:00:40 +0800

    Add files via upload
    For Update.py file. If user intends to change the information or an incorrect information is placed, this can be command can be used.

---
+++ Update_additionalEnlisting.py
@@ -0,0 +1,13 @@
+    def update_db(self):
+        # declaring the variables to update
+        self.var1 = self.ent1.get() #updated name
+        self.var2 = self.ent2.get() #updated age
+        self.var3 = self.ent3.get() #updated gender
+        self.var4 = self.ent4.get() #updated location
+        self.var5 = self.ent5.get() #updated phone
+        self.var6 = self.ent6.get() #updated time
+
+        query = "UPDATE appointments SET name=?, age=?, gender=?, location=?, phone=?, scheduled_time=? WHERE name LIKE ?"
+        c.execute(query, (self.var1, self.var2, self.var3, self.var4, self.var5, self.var6, self.namenet.get(),))
+        conn.commit()
+        tkMessageBox.showinfo("Updated", "Successfully Updated.")
